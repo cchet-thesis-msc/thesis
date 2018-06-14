1153ab777591d02020902c27b2df516d34094f43
Thomas Herzog <herzog.thomas81@gmail.com>
2018-06-14 12:26:28 +0200

    - further dev on impl part

---
+++ thesis/sources/java-config-inject.java
@@ -0,0 +1,11 @@
+@Inject
+@ConfigProperty(name = "keycloak.token-url")
+private String keycloakTokenUrl;
+
+@Inject
+@ConfigProperty(name = "keycloak.client.id")
+private String keycloakClientId;
+
+@Inject
+@ConfigProperty(name = "keycloak.client.secret")
+private String keycloakClientSecret;
