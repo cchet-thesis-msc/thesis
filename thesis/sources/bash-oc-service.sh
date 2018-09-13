c2f27a589d47cf8d9ec673126a5f3c1f12db1cb0
Thomas Herzog <herzog.thomas81@gmail.com>
2018-09-13 19:56:15 +0200

    - Little changes

--- thesis/sources/bash-oc-service.sh
+++ thesis/sources/bash-oc-service.sh
@@ -1,12 +1,20 @@
 function createSecrets() {
   oc create secret generic ${SECRET_SERVIVE} \
-    --from-env-file=./config.properties
+    --from-env-file=./config/${STAGE}/config.properties
 
   oc create secret generic ${SECRET_SERVIVE_KEYCLOAK} \
-    --from-file=./keycloak.json
+    --from-file=./config/${STAGE}/keycloak.json
 } # createSecrets
 
 function deleteSecrets() {
   oc delete secret/${SECRET_SERVIVE}
   oc delete secret/${SECRET_SERVIVE_KEYCLOAK}
-} # deleteSecrets
+} # createSecrets
+
+function scaleUp() {
+  oc scale --replicas=1 dc/${SERVICE_NAME}
+} # scaleUp
+
+function scaleDown() {
+  oc scale --replicas=0 dc/${SERVICE_NAME}
+} # scaleDown
--- thesis/sources/java-logging-tracing-id.java
+++ thesis/sources/java-logging-tracing-id.java
@@ -2,16 +2,11 @@
 public static class MDCContainerRequestFilter
   implements ContainerRequestFilter {
 
-  @Inject
-  private Instance<Scope> scopeInstance;
-
   @Override
   public void filter(ContainerRequestContext requestContext)
     throws IOException {
-    // Uber specific format 'aaa:ffff:0:1'
-    final String tracingId = scopeInstance.get().span()
-                              .context().toString()
-                              .split(":")[0];
+    final String tracingId = GlobalTracer.get().scopeManager().active()
+                              .span().context().toString().split(":")[0];
     MDC.put("transaction.id", tracingId);
   }
 }
